6a314679ff428579be1270afdae478a62880dbd8
LiJohnson <598420668@163.com>
2017-07-17 01:58:58 +0800

    调整外部资源引用

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var album = require('../icloud-share-album');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //album('B0Z5qXGF1iS2iW').then( data=>{});
-  res.render('index', { title:"2017-07-16" });
+  res.render('index', { title:"2017-07-16",music:req.query.music });
 });
 
 router.all('/album/:album',function( req , res , next ){
